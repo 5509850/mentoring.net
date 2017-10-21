71c808c6f62e3058270c994dbc2719322ac587c0
Aliaksandr Harbunou <5509850@gmail.com>
2017-10-21 23:18:13 +0300

    Module 1 Async-Await task 2

---
+++ Module 1 Async-Await 2/MainForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Module_1_Async_Await_2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_help = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label_error = new System.Windows.Forms.Label();
+            this.button_download = new System.Windows.Forms.Button();
+            this.textBox_URL = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label_count = new System.Windows.Forms.Label();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_help
+            // 
+            this.button_help.Location = new System.Drawing.Point(476, 3);
+            this.button_help.Name = "button_help";
+            this.button_help.Size = new System.Drawing.Size(48, 23);
+            this.button_help.TabIndex = 0;
+            this.button_help.Text = "reame";
+            this.button_help.UseVisualStyleBackColor = true;
+            this.button_help.Click += new System.EventHandler(this.button_help_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label_error);
+            this.groupBox1.Controls.Add(this.button_download);
+            this.groupBox1.Controls.Add(this.textBox_URL);
+            this.groupBox1.Location = new System.Drawing.Point(13, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(511, 76);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "адрес файла для закачки";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(455, 46);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "git";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(455, 16);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "office";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(392, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "aimp";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(392, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "7zip";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label_error
+            // 
+            this.label_error.AutoSize = true;
+            this.label_error.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_error.ForeColor = System.Drawing.Color.Red;
+            this.label_error.Location = new System.Drawing.Point(114, 56);
+            this.label_error.Name = "label_error";
+            this.label_error.Size = new System.Drawing.Size(74, 13);
+            this.label_error.TabIndex = 2;
+            this.label_error.Text = "Invalid URL";
+            this.label_error.Visible = false;
+            // 
+            // button_download
+            // 
+            this.button_download.Location = new System.Drawing.Point(7, 47);
+            this.button_download.Name = "button_download";
+            this.button_download.Size = new System.Drawing.Size(100, 23);
+            this.button_download.TabIndex = 1;
+            this.button_download.Text = "Download";
+            this.button_download.UseVisualStyleBackColor = true;
+            this.button_download.Click += new System.EventHandler(this.button_download_Click);
+            // 
+            // textBox_URL
+            // 
+            this.textBox_URL.Location = new System.Drawing.Point(7, 20);
+            this.textBox_URL.Name = "textBox_URL";
+            this.textBox_URL.Size = new System.Drawing.Size(379, 20);
+            this.textBox_URL.TabIndex = 0;
+            this.textBox_URL.Text = "http://";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(13, 112);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(511, 248);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "закачки";
+            // 
+            // label_count
+            // 
+            this.label_count.AutoSize = true;
+            this.label_count.Location = new System.Drawing.Point(20, 12);
+            this.label_count.Name = "label_count";
+            this.label_count.Size = new System.Drawing.Size(34, 13);
+            this.label_count.TabIndex = 4;
+            this.label_count.Text = "count";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(0, 0);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(100, 23);
+            this.progressBar2.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 369);
+            this.Controls.Add(this.label_count);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button_help);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Менеджер закачек";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_help;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_URL;
+        private System.Windows.Forms.Button button_download;
+        private System.Windows.Forms.Label label_error;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label_count;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Module 1 Async-Await 2/MainForm.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module_1_Async_Await_2
+{
+    public partial class MainForm : Form
+    {            
+        private int count = 0;       
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button_help_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(@"Напишите простейший менеджер закачек. 
+Пользователь задает адрес страницы, которую необходимо загрузить. 
+В процессе загрузки пользователь может ее отменить. 
+Пользователь может задавать несколько источников для закачки. 
+Скачивание страниц не должно блокировать интерфейс приложения.", "Описание программы",MessageBoxButtons.OK , MessageBoxIcon.Information);
+        }
+
+        private void button_download_Click(object sender, EventArgs e)
+        {
+            var uiContext = SynchronizationContext.Current;
+            if (Vaidate(textBox_URL.Text))
+            {
+                label_error.Visible = false;
+                var ts = new CancellationTokenSource();
+                CancellationToken ct = ts.Token;
+                Guid guid = Guid.NewGuid();                
+                DownLoadPage(textBox_URL.Text, ct, guid, uiContext);
+                AddProgressBar(textBox_URL.Text, ts, guid);
+                textBox_URL.Text = "http://";
+            }
+            else
+            {
+                label_error.Visible = true;
+                textBox_URL.Text = "http://";
+            }
+        }          
+
+        private async void DownLoadPage(string url, CancellationToken ct, Guid guid, SynchronizationContext uiContext)
+        {
+            var split = url.Split('/');
+            string saveFolder = Path.Combine(Directory.GetCurrentDirectory(), "Download");
+            Directory.CreateDirectory(saveFolder);
+            string savelocation = Path.Combine(saveFolder, split[split.Length - 1]);
+            await Task.Factory.StartNew(() =>
+            {
+                using (WebClient client = new WebClient())
+                {
+                    Task.Delay(1500);
+                    if (ct.IsCancellationRequested)
+                    {
+                            return;
+                    }
+                    try
+                    {
+                        client.DownloadFile(new Uri(url), savelocation);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                                        
+                    if (ct.IsCancellationRequested)
+                    {                       
+                        if (File.Exists(savelocation))
+                        {
+                            File.Delete(savelocation);
+                        }
+                    }
+                    else
+                    {
+                        uiContext.Post(Complete, guid);                       
+                    }
+                }
+            }, ct);           
+        }
+    
+        private void Complete(object obj)
+        {            
+            try
+            {
+                Guid guid = (Guid)obj;
+                bool alldownloaded = true;
+                foreach (Control ctrl in groupBox2.Controls)
+                {
+                    if (ctrl.Name.Contains("progressBar"))
+                    {
+                        var prB = (CustomProgressBar)ctrl;
+                        if (ctrl.Name.Equals($"progressBar{guid}"))
+                        {                           
+                            prB.Value = 100;                         
+                        }
+                        if (prB.Value != 100)
+                        {
+                            alldownloaded = false;
+                        }
+                    }                    
+                }
+                if (alldownloaded)
+                {
+                    var saveFolder = Path.Combine(Directory.GetCurrentDirectory(), "Download");
+                    System.Diagnostics.Process.Start("explorer.exe", saveFolder);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }            
+        }
+
+        private void AddProgressBar(string url, CancellationTokenSource ts, Guid guid)
+        {
+            count++;
+            int Y = count * 19;
+            label_count.Text = $"count = {count}";
+            int progressX = 10;
+            int buttonX = 474;
+
+            Button btn = new Button()
+            {
+                BackColor = Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0))))),
+                Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(204))),
+                Location = new Point(buttonX, Y),
+                Name = guid.ToString(),
+                Size = new Size(31, 23),
+                Text = "X",
+                UseVisualStyleBackColor = false,
+                Tag = ts
+            };
+            btn.Click += Btn_Click;
+            groupBox2.Controls.Add(btn);
+            CustomProgressBar progressBar = new CustomProgressBar()
+            {
+                Location = new Point(progressX, Y),
+                Name = $"progressBar{guid}",
+                Size = new Size(458, 23),
+                DisplayStyle = ProgressBarDisplayText.CustomText,
+                CustomText = url
+            };
+            progressBar.Value = 20;
+            groupBox2.Controls.Add(progressBar);
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Guid index;           
+            Button button = (Button)sender;
+            Guid.TryParse(button.Name, out index);
+            CancellationTokenSource ts = (CancellationTokenSource)button.Tag;
+            if (ts != null)
+            {
+                ts.Cancel();
+            }
+            CustomProgressBar prB = null;
+            Button btn = null;
+            foreach (Control ctrl in groupBox2.Controls)
+                {
+                    if (ctrl.Name.Equals($"{index}"))
+                    {
+                        btn = (Button)ctrl;
+                        btn.Click -= Btn_Click;
+                    }
+                    if (ctrl.Name.Equals($"progressBar{index}"))
+                    {
+                        prB = (CustomProgressBar)ctrl;                        
+                    }                    
+                }
+                if (prB != null && btn != null)
+                {
+                    groupBox2.Controls.Remove(prB);
+                    groupBox2.Controls.Remove(btn);
+                    ReBuildControl();
+                }            
+        }
+
+        private void ReBuildControl()
+        {
+            count = 0;                    
+            int progressX = 10;
+            int buttonX = 474;
+            List<Control> contrlols = new List<Control>();
+            foreach (Control ctrl in groupBox2.Controls)
+            {
+                contrlols.Add(ctrl);
+            }
+            groupBox2.Controls.Clear();
+            int Y = 0;
+            int line = 0;
+            foreach (Control ctrl in contrlols)
+            {
+                line++;
+                Y = (count + 1) * 19;
+                if (line % 2 != 0)
+                {
+                    ctrl.Location = new Point(buttonX, Y);
+                }
+                else
+                {
+                    ctrl.Location = new Point(progressX, Y);
+                    count++;
+                }
+                groupBox2.Controls.Add(ctrl);
+            }
+            label_count.Text = $"count = {count}";
+        }
+
+        private bool Vaidate(string uriName)
+        {
+            if (string.IsNullOrEmpty(uriName))
+            {
+                return false;
+            }
+            if (!Uri.IsWellFormedUriString(uriName, UriKind.RelativeOrAbsolute))
+            {
+                return false;
+            }
+            foreach (Control ctrl in groupBox2.Controls)
+            {
+                if (ctrl.Name.Contains("progressBar"))
+                {
+                    var prB = (CustomProgressBar)ctrl;
+                    if (prB.Value != 100 && prB.CustomText.Equals(uriName))
+                    {
+                        MessageBox.Show("Duplicate URL!");
+                        return false;
+                    }
+                }
+            }
+            var split = uriName.Split('/');
+            if (split == null || split.Length < 2)
+            {
+                return false;
+            }
+            string saveFolder = Path.Combine(Directory.GetCurrentDirectory(), "Download");
+            string savelocation = Path.Combine(saveFolder, split[split.Length - 1]);
+            if (File.Exists(savelocation))
+            {
+                try
+                {
+                    File.Delete(savelocation);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Wait for Cancel!");
+                    return false;
+                }
+                if (File.Exists(savelocation))
+                {
+                    MessageBox.Show("Wait for Cancel!");
+                    return false;
+                }
+            }
+            return true; ;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox_URL.Text = "http://7-zip.org/a/7z1701-x64.exe";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox_URL.Text = "https://downloads.sourceforge.net/project/openofficeorg.mirror/4.1.3/binaries/ru/Apache_OpenOffice_4.1.3_Win_x86_install_ru.exe";            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox_URL.Text = "http://aimp.su/storage/aaccd58e9dc040471baec47c976e1b75/aimp_4.13.1897.exe";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox_URL.Text = "https://download.tortoisegit.org/tgit/2.5.0.0/TortoiseGit-2.5.0.0-64bit.msi";
+            
+        }
+    }
+}
